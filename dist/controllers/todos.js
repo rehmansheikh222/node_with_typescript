2edc33f7b124deaa08c7b7e7643be962e43815d0
Rehman Sheikh <sh.rehman222@gmail.com>
2023-08-29 18:13:33 +0500

    Fetch and Updated todo

--- dist/controllers/todos.js
+++ dist/controllers/todos.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTodo = void 0;
+exports.updateTodo = exports.getTodod = exports.createTodo = void 0;
 const todo_1 = require("../models.ts/todo");
 // export const createTodo = (req: Request, res: Response, next: NextFunction) => {}
 const TODOS = [];
@@ -11,3 +11,18 @@ const createTodo = (req, res, next) => {
     res.status(201).json({ message: 'Created the todo', createdTodo: newTodo });
 };
 exports.createTodo = createTodo;
+const getTodod = (req, res, next) => {
+    res.status(200).json({ todos: TODOS });
+};
+exports.getTodod = getTodod;
+const updateTodo = (req, res, next) => {
+    const todoId = req.params.id;
+    const updatedText = req.body.text;
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+    if (todoIndex < 0) {
+        throw new Error('Could not find todo!!');
+    }
+    TODOS[todoIndex].text = updatedText;
+    res.status(201).json({ message: "Updated", updatedTodo: TODOS[todoIndex] });
+};
+exports.updateTodo = updateTodo;
--- src/routes/todos.ts
+++ src/routes/todos.ts
@@ -1,10 +1,10 @@
 import {Router} from 'express'
-import { createTodo } from '../controllers/todos'
+import { createTodo, getTodod, updateTodo } from '../controllers/todos'
 const router = Router()
 
 router.post('/', createTodo)
-router.get('/')
-router.patch('/:id')
+router.get('/', getTodod)
+router.patch('/:id', updateTodo)
 router.delete('/:id')
 
 export default router
