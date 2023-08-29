fb7adb96d56cc133c15bd131fd1bc278fe3ee934
Rehman Sheikh <sh.rehman222@gmail.com>
2023-08-29 17:50:42 +0500

    Adding milddleware types

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
-import express from 'express'
-
+import express, { NextFunction, Request, Response } from 'express'
+import todoRoutes from './routes/todos'
 const app = express()
+
+app.use('/todos', todoRoutes)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction): any => {
+    res.status(500).json({message: err.message})
+})
 
 app.listen(3000)
